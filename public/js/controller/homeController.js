e669c650ace45f85ad3900e3a91b0309e1004f4d
sathyanarayanan310 <sathyajothi007@gmail.com>
2017-02-09 18:50:36 +0530

    Add files via upload

--- public/js/controller/homeController.js
+++ public/js/controller/homeController.js
@@ -4,3 +4,11 @@ module.exports = function($scope) {
   $scope.home = 'homepage';
 console,log($scope.home);
 };
+
+var refresh = function() {
+    $http.get('/movie/movie').success(function(response) {
+        console.log('READ IS SUCCESSFUL');
+        $scope.moviList = response;
+        $scope.movi = "";
+    });
+};
