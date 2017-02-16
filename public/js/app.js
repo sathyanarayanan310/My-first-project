5209f7921ac6fa62c8c3aa6d08d8b02538181102
sathyanarayanan310 <sathyajothi007@gmail.com>
2017-02-16 19:29:32 +0530

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,37 @@ var app = angular.module('movieApp', [ 'ngRoute' ]);
 
 require('./service');
 require('./controller');
+app.filter('unique', function() {
+
+   return function(collection, keyname) {
+
+      var output = [],
+          keys = [];
+
+
+      angular.forEach(collection, function(item) {
+
+          var key = item[keyname];
+
+          if(keys.indexOf(key) === -1) {
+
+              keys.push(key);
+
+              output.push(item);
+          }
+      });
+
+      return output;
+   };
+});
+// var uniqueNames = [];
+//   var uniqueObj = [];
+//    for(i = 0; i< data.length; i++){
+//     if(uniqueNames.indexOf(data[i].City) === -1){
+//     uniqueObj.push(data[i])
+//     uniqueNames.push(data[i].City);
+//   }
+// }
 
 app.config(function($routeProvider) {
 
@@ -19,16 +50,21 @@ app.config(function($routeProvider) {
     controller: 'BookingController',
   })
   .when('/cancellation', {
-    templateUrl: 'views/cancellation.html',
+    templateUrl: 'views/sample.html',
     controller: 'CancellationController',
   })
   .when('/admin', {
     templateUrl: 'views/admin.html',
     // controller: 'CancellationController',
   })
+  .when('/sample', {
+    templateUrl: 'views/sample.html',
+    // controller: 'CancellationController',
+  })
   .when('/moviebooking', {
     templateUrl: 'views/moviebooking.html',
     controller: 'MovieBookingController',
+
   })
   .otherwise({
     redirectTo: 'views/home.html',
