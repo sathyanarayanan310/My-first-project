7af3ab8a1afa312a134d9b5cf7f09dfb325dc3f1
sathyanarayanan310 <sathyajothi007@gmail.com>
2017-02-28 19:19:52 +0530

    Add files via upload

--- routes/movie-crud.js
+++ routes/movie-crud.js
@@ -5,105 +5,6 @@ var bodyParser=require('body-parser');
 router.use(bodyParser.urlencoded({extended:true}));
 var mongoose = require('mongoose');
 
-//
-//
-// var movieSchema = mongoose.Schema({
-// Title:String,
-// Year:Number,
-// Language:String,
-// Poster:String,
-// Genre:String,
-// Director:String,
-// Actors:String,
-// Runtime:String,
-//
-//   // :String,
-//   // moviePlot:String,
-//   // Poster:String,
-// });
-//
-// var Movie = mongoose.model('Movie', movieSchema, 'moviecrud');
-//
-// // router.get('/movie/get', function (req, res,next) {
-// //     console.log("REACHED GET FUNCTION ON SERVER");
-// //     Movie.find({}, function (err, docs) {
-// //          res.json(docs);
-// //
-// //     });
-// // });
-// router.get('/movie/get/', function (req,res,next) {
-//
-//     console.log("REACHED  GET omdb FUNCTION ON SERVER");
-//     Movie.find({}, function (err, docs)
-//     {
-//          res.json(docs);
-//          console.log(docs);
-//
-//     });
-// });
-//
-// router.get('/movie/:id', function (req,res) {
-//     console.log("REACHED GET ID FUNCTION ON SERVER");
-//      Movie.find({_id: req.params.id}, function (err, docs) {
-//          res.json(docs);
-//
-//     });
-// });
-//
-// router.post('/movie', function(req, res){
-//   console.log(req.body);
-//   var name = req.body.Title;
-//     var year = req.body.Year;
-//     var lang = req.body.Language;
-//     var post = req.body.Poster;
-//    var genre = req.body.Genre;
-//    var dir = req.body.Director;
-//    var act = req.body.Actors;
-//
-//   var runtime = req.body.Runtime;
-//
-// var movie1 = new Movie({
-//     Title:name,
-//     Year:year,
-//     Language:lang,
-//     Poster:post,
-//     Genre:genre,
-//     Director:dir,
-//     Actors:act,
-//     Runtime:runtime
-//   });
-//
-//   movie1.save(function(err, docs){
-//     if ( err ) throw err;
-//     console.log("movie Saved Successfully");
-//     res.json(docs);
-//   });
-//   })
-//
-// router.delete('/movie/:id', function(req, res){
-//    console.log("REACHED Delete FUNCTION ON SERVER");
-//       Movie.remove({_id:req.params.id}, function(err, docs){
-//         res.json(docs);
-//     });
-// })
-//
-// router.put('/movie/:id', function(req, res){
-//     console.log("REACHED PUT");
-//     console.log(req.body);
-//     Movie.findOneAndUpdate({_id:req.params.id}, req.body, function (err, data) {
-//       res.json(data);
-//     });
-// })
-//
-// router.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// module.exports = router;
-
-
 
 var movieSchema = mongoose.Schema({
    moviID: String,
@@ -120,16 +21,7 @@ moviRating:String
 
 var Movie = mongoose.model('Movie', movieSchema, 'movie');
 
-// router.get('/movie/get/', function (req, res,next) {
-//
-//     console.log("REACHED  GET omdb FUNCTION ON SERVER");
-//     Movie.find({}, function (err, docs)
-//     {
-//          res.json(docs);
-//          console.log(docs);
-//
-//     });
-// });
+
 
 
 router.get('/movie', function (req, res,next) {
